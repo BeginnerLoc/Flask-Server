95babdcfee445e50a1efa92090654769a32b27d5
tienloctran <loctientran235@gmail.com>
2023-05-05 14:37:45 +0800

    add /api/most_incients route - test data
    Data to be retrieved from MongoDB

--- ai_server/routes.py
+++ ai_server/routes.py
@@ -6,3 +6,8 @@ main = Blueprint("main", __name__)
 def index():
     return render_template("index.html")
 
+# return JSON containing the incient with most cases and the number of cases
+@main.route("/api/most_incidents")
+def most_incidents():
+    response =  {'breach': 'no helmet', 'incident_count': 100}
+    return jsonify(response)
