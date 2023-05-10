1fc4429a8ef677f131b3ec5648808b49d21b6efc
tienloctran <loctientran235@gmail.com>
2023-05-10 15:25:04 +0800

    add and connect to mongodb

--- ai_server/extensions.py
+++ ai_server/extensions.py
@@ -1,3 +1,15 @@
 from flask_socketio import SocketIO
+from pymongo.mongo_client import MongoClient
 
+uri = "mongodb+srv://loctientran235:PUp2XTv7tkArDjJB@c290.5lmj4xh.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+db_client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    db_client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+    
 socketio = SocketIO(async_mode="eventlet", cors_allowed_origins="*")
