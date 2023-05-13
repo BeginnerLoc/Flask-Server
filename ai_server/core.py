3424286485563de26adb9aac3c4ea4468e5d483c
tienloctran <loctientran235@gmail.com>
2023-05-13 16:30:15 +0800

    load multiple encodings from database to detect multiple faces

--- ai_server/core.py
+++ ai_server/core.py
@@ -30,27 +30,24 @@ def save_encodings(encoding):
 
 
 def retrieve_encoding():
-    # retrieve the document from the collection
-    encoding_data = collection.find_one()
+    # retrieve all the documents from the collection
+    encoding_data = collection.find(sort=[('_id', -1)])
 
-    return encoding_data    
+    # create a list to store the encodings
+    encoding_list = []
+    for encoding in encoding_data:
+        num_vals = encoding['encode'][1:]
+        detect_name = encoding['encode'][0]
+        encode_np = np.array(num_vals)
+        encoding_list.append((detect_name, encode_np))
+        
+    return encoding_list   
     
 def recognition():
-    encoding_data = retrieve_encoding()
+    encoding_list = retrieve_encoding()
     
-    num_vals = encoding_data['encode'][1:]
-    detect_name = encoding_data['encode'][0]
-    
-    encode_np = np.array(num_vals)
-
-    
-    known_face_encodings = [
-        encode_np
-    ]
-    
-    known_face_names = [
-        detect_name
-    ]
+    known_face_encodings = [encoding[1] for encoding in encoding_list]
+    known_face_names = [encoding[0] for encoding in encoding_list]
     
     face_locations = []
     face_encodings = []
@@ -79,16 +76,17 @@ def recognition():
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                 name = "Unknown"
 
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
+                # If a match was found in known_face_encodings, just use the first one.
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = known_face_names[first_match_index]
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+                else:
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
 
                 face_names.append(name)
 
@@ -104,12 +102,11 @@ def recognition():
             left *= 4
 
             # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            if name != "Unknown":
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Display the resulting image
         cv2.imshow('Video', frame)
@@ -125,7 +122,7 @@ def recognition():
 
 
 
-encoding = train_encoding("daren.jpg")
-save_encodings(encoding)
+# encoding = train_encoding("Tai.1.jpg")
+# save_encodings(encoding)
 video_capture = cv2.VideoCapture(0)
 recognition()
