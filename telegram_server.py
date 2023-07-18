5897a33b07f9689f2a99284949d0d7af67722bb7
TheOnlyAstro <iknowastro@gmail.com>
2023-07-18 18:03:38 +0800

    Added logging, breach functions and retry function
    Added logging, breach functions and retry function

--- telegram_server.py
+++ telegram_server.py
@@ -6,16 +6,19 @@ import base64
 import io
 from PIL import Image
 from tenacity import retry, stop_after_attempt, wait_fixed
+import logging
 
 bot_token = '6060060457:AAGRyic-1HVFcUy1dSEsdLMJo0rB9Mvz0y0'
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
 async def reply_to_message(bot: Bot, update: Update):
     user_id = update.effective_user.id
     message_text = update.message.text.lower()
     chat_id = update.effective_chat.id
     response_text = ""
     chat_name = update.effective_user.first_name 
+    logging.info(f"User {chat_name} typed {message_text}")
 
     if not message_text.startswith('/'):
         return
@@ -32,11 +35,13 @@ async def reply_to_message(bot: Bot, update: Update):
         /breachList - Use this to see the last 20 incidents that had occurred. (i.e. /breachList) \n
         /breach <Number> - Use this to view an incident using the incident ID. (i.e. /breach 100) \n
         /resolveBreach <Number> <Action> - Use this to resolve an incident using the incident ID. (i.e. /resolve 100 Verbal Warning was given.) \n
+        /unresolvedbreach - Use this to view the unresolved breaches. \n
                               
         -- Incidents -- \n
         /hazardList - Use this to see the last 20 hazards that had occurred. (i.e. /hazardList) \n
         /hazard <Number> - Use this to view a hazard using the hazard ID. (i.e. /hazard 100) \n
-        /resolveHazard - Use this to resolve a hazard using the hazard ID. (i.e. /resolve 100 Hazard was removed.)            
+        /resolveHazard - Use this to resolve a hazard using the hazard ID. (i.e. /resolve 100 Hazard was removed.)\n    
+        /unresolvedHazard - Use this to view the unresolved hazards. 
         """)
 
     # View the last 20 breaches that had happened
@@ -48,21 +53,34 @@ async def reply_to_message(bot: Bot, update: Update):
                 breach_id = breach.get("breach_id", "")
                 name = breach.get("name", "")
                 breach_type = ", ".join(breach.get("breach_type", []))
-                timestamp = breach.get("timestamp", "")
-                locations = breach.get("location", "")
-                case_resolved = breach.get("case_resolved", "")
 
                 response_text += textwrap.dedent(f"""\
                 Breach ID: {breach_id}
                 Name: {name}
                 Breach Type: {breach_type}
-                Timestamp: {timestamp}
-                Location: {locations}
-                Case Resolved: {case_resolved}\n
                 """)
 
         else:
             response_text = "No breaches found."
+
+    # View the list of unresolved breaches that had happened
+    elif message_text == '/unresolvedbreach':
+        breaches = await viewUnresolved("breaches")
+        if breaches:
+            response_text = "Unresolved Breaches:\n\n"
+            for breach in breaches:
+                breach_id = breach.get("breach_id", "")
+                name = breach.get("name", "")
+                breach_type = ", ".join(breach.get("breach_type", []))
+
+                response_text += textwrap.dedent(f"""\
+                Breach ID: {breach_id}
+                Name: {name}
+                Breach Type: {breach_type}\n
+                """)
+        else:
+            response_text = "Congrats! There is no unresolved cases for now!."
+
 
     #View the specific breach with the specific ID
     elif message_text.startswith('/breach'):
@@ -136,13 +154,62 @@ async def reply_to_message(bot: Bot, update: Update):
         else:
             response_text = "Invalid command format. Please use /resolveBreach <Number> <Action>."
 
-    # TODO: Implement /hazardList to view the last 20 hazards
-    elif message_text == '/hazardList':
-        print("Hazard List!")
+    # View the last 20 hazards that had happened
+    elif message_text == '/hazardlist':
+        hazards = await hazardList()
+        if hazards:
+            response_text = "Last 20 Hazards:\n\n"
+            for hazard in hazards:
+                hazard_id = hazard.get("hazard_id", "")
+                hazard_type = hazard.get("item")
+                case_resolved = hazard.get("case_resolved", "")
+
+                response_text += textwrap.dedent(f"""\
+                Breach ID: {hazard_id}
+                Hazard Type: {hazard_type}
+                Case Resolved: {case_resolved}\n
+                """)
+
+        else:
+            response_text = "No breaches found."
     
     # TODO: Implement /hazard to view the specific hazard with the specific ID
     elif message_text.startswith('/hazard'):
-        print("Hazard!")
+        # Extract the incident ID from the message text
+        hazard_id = int(message_text.split(' ')[-1])
+        document = await search_mongo_id("hazards", hazard_id)
+        if document :
+            name = document["item"]
+            encoded_image = document["image"]
+            timestamp = document["timestamp"]
+            locations = document["location"]
+            hazard_id = document["hazard_id"]
+            case_resolved = document["case_resolved"]
+
+            response_text = textwrap.dedent(f"""\
+            Hazard ID: {hazard_id}
+            Hazard Type: {name}
+            Timestamp: {timestamp}
+            Location: {locations}
+            Case Resolved: {case_resolved}""")
+
+
+            if encoded_image:
+                decoded_image = base64.b64decode(encoded_image)
+                image_io = io.BytesIO(decoded_image)
+                image = Image.open(image_io)
+
+                # Convert the image to JPEG format
+                jpeg_image_io = io.BytesIO()
+                image.save(jpeg_image_io, 'JPEG')
+                jpeg_image_io.seek(0)
+
+                # Send the image via the Telegram message
+                await bot.send_photo(chat_id=chat_id, photo=jpeg_image_io, caption=response_text)
+                return
+
+        else:
+            response_text = "The ID that you have input is invalid"
 
     # Resolve the hazard using /resolvehazard <Number> <Action> which would be updated in the database
     elif message_text.startswith('/resolvehazard'):
@@ -166,13 +233,31 @@ async def reply_to_message(bot: Bot, update: Update):
         else:
             response_text = "Invalid command format. Please use /resolveHazard <Number> <Action>."
 
+    elif message_text == '/unresolvedhazard':
+        hazards = await viewUnresolved("hazards")
+        if hazards:
+            response_text = "Unresolved Hazards:\n\n"
+            for hazard in hazards:
+                hazard_id = hazard.get("hazard_id", "")
+                hazard_type = hazard.get("item")
+                case_resolved = hazard.get("case_resolved", "")
+
+                response_text += textwrap.dedent(f"""\
+                Breach ID: {hazard_id}
+                Hazard Type: {hazard_type}
+                Case Resolved: {case_resolved}\n
+                """)
+        else:
+            response_text = "Congrats! There is no unresolved cases for now!."
+
     else:
         response_text = f"Invalid Action or Command. Please use /Commandlist to get a list of valid commands."
 
     if response_text:
         await bot.send_message(chat_id=chat_id, text=response_text)
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
 async def search_mongo_id(type, id):
     # Connect to MongoDB
     client = pymongo.MongoClient("mongodb+srv://Astro:enwVEQqCyk9gYBzN@c290.5lmj4xh.mongodb.net/")
@@ -193,7 +278,27 @@ async def search_mongo_id(type, id):
     else:
         return None  # No document found
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
+async def viewUnresolved(type):
+    client = pymongo.MongoClient("mongodb+srv://Astro:enwVEQqCyk9gYBzN@c290.5lmj4xh.mongodb.net/")
+    db = client["construction"]
+
+    if type == "breaches":
+        collection = db["breach_images"]
+    elif type == "hazards":
+        collection = db["hazards_1"]
+    else:
+        return None
+    document = collection.find({"case_resolved": False}).sort([("timestamp", pymongo.DESCENDING)]).limit(20)
+
+    if document:
+        return document
+    else:
+        return None  # No document found
+
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
 async def changeResolved(type, id, reason):
         # Connect to MongoDB
     client = pymongo.MongoClient("mongodb+srv://Astro:enwVEQqCyk9gYBzN@c290.5lmj4xh.mongodb.net/")
@@ -223,7 +328,8 @@ async def changeResolved(type, id, reason):
         print(f"Connection failed: {e}")
         return False  # Return False if the update fails
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
 async def breachList():
     try:
         client = pymongo.MongoClient("mongodb+srv://Astro:enwVEQqCyk9gYBzN@c290.5lmj4xh.mongodb.net/")
@@ -235,10 +341,26 @@ async def breachList():
         print(f"Connection failed: {e}")
         return None
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
+async def hazardList():
+    try:
+        client = pymongo.MongoClient("mongodb+srv://Astro:enwVEQqCyk9gYBzN@c290.5lmj4xh.mongodb.net/")
+        db = client["construction"]
+        collection = db["hazards_1"]
+        hazards = collection.find().sort([("timestamp", pymongo.DESCENDING)]).limit(20)
+        return list(hazards)
+    except pymongo.errors.ConnectionFailure as e:
+        print(f"Connection failed: {e}")
+        return None
+
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(5))
 async def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     bot = Bot(token=bot_token)
     offset = 0 
+    
 
     while True:
         try:
@@ -250,6 +372,7 @@ async def main():
             await asyncio.sleep(1)
         except Exception as e:
             print(f"An error occurred: {e}")
+            logging.exception("Error occurred during update processing.")
             print("Try again later!")
 
 
